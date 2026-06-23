44fb08bdd867c93c5ef574f996456e1b5adcdeb4
Melodyysol <issaabdulwaris212@gmail.com>
2026-06-23 09:59:54 +0100

    Set up react, daisyUi and tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-base-200">
+  <div className="bg-base-100 border-base-300 text-base-content">
+    This is dark text on a light background, which switches to light text on a dark background in
+    dark mode.
+  </div>
+</div>
+  );
+}
+
+export default App;
